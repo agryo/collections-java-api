422237ad3dd2987f283cec066889d4c0508a1f0b
Agryo Araujo <agryostallion@gmail.com>
2023-08-24 00:01:03 -0300

    Resolução do desafio e testes dos métodos

--- src/main/java/set/Ordenacao/Main.java
+++ src/main/java/set/Ordenacao/Main.java
@@ -14,5 +14,22 @@ public class Main {
         System.out.println(cadastroProdutos.exibirProdutosPorNome());
 
         System.out.println(cadastroProdutos.exibirProdutosPorPreco());
+
+        System.out.println("Segunda tarefa do desafio");
+        GerenciadorAlunos gerenciadorAlunos = new GerenciadorAlunos();
+
+        gerenciadorAlunos.adicionarAluno("Aluno 1", 123456l, 6d);
+        gerenciadorAlunos.adicionarAluno("Aluno 2", 1234567l, 8d);
+        gerenciadorAlunos.adicionarAluno("Aluno 3", 12345678l, 1d);
+        gerenciadorAlunos.adicionarAluno("Aluno 4", 123456789l, 9d);
+        System.out.println(gerenciadorAlunos.alunosSet);
+
+        gerenciadorAlunos.removerAlunoPorMatricula(000l);
+        gerenciadorAlunos.removerAlunoPorMatricula(123456789l);
+        System.out.println(gerenciadorAlunos.alunosSet);
+
+        gerenciadorAlunos.exibirAlunosPorNome();
+
+        gerenciadorAlunos.exibirAlunosPorNota();
     }
 }
